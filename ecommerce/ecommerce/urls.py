2e34d224704abf3af4cd4db1abf780ea4e4b6d32
sharik10ahmed <sharikahmed757@gmail.com>
2026-07-18 23:39:30 +0530

    feat: Add favicon support and enhance product detail feedback display

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicons/favicon.ico')),
     path('',include('PickUp.urls')),
 ]
 
